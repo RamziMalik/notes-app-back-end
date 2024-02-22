e696b7d079b5ae04b3bb1049751f2a896ac86f32
Ramzi Malik <ramzimalik07@gmail.com>
2024-02-22 20:00:31 +0700

    Fix typo

--- src/handler.js
+++ src/handler.js
@@ -89,7 +89,7 @@ const editNoteByIdHandler = (request, h) => {
 
     const response = h.response({
       status: 'success',
-      message: 'Catata berhasil diperbarui',
+      message: 'Catatan berhasil diperbarui',
     });
     response.code(200);
     return response;
